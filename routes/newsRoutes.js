94c94cb1e4e0cb8bb9239a6fe3ffbf3284f69b20
Thilina Thushara <thilina.sam.a7@gmail.com>
2025-08-16 15:32:54 +0530

    add dummy news routes

--- routes/newsRoutes.js
+++ routes/newsRoutes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const newsController = require('../controllers/newsController')
 
-router.get('/',(req,res)=>{
-    res.json({message:'OK'})
-})
+router.get('/', newsController.getNews)
 
 module.exports = router;
