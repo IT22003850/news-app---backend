276dc55b325ad446a5f8164734f1723b8a038ffd
Thilina Thushara <thilina.sam.a7@gmail.com>
2025-08-16 16:00:20 +0530

    impliment dummy postnews controller function

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -6,8 +6,29 @@ const getNews = async (req, res) => {
         res.json(news);
     } catch (error) {
         console.log(`Error Fetching News: ${error}`.red);
-        res.status(500).json({message: 'Server error, please try again later..!'})
+        res.status(500).json({message: 'Server error, please try again later!'})
     }
 }
 
-module.exports = {getNews};
+const postNews = async (req, res) => {
+    try {
+        const {title, description} = req.body;
+
+        if(!title || !description){
+            return res.status(400).json({message: "title and description are required fields!"})
+        }
+
+        const news = await News.create({
+            title,
+            description,
+        });
+
+        res.status(201).json(news);
+        
+    } catch (error) {
+        console.log(`Error creating news: ${error}`);
+        res.status(500).json({message: 'Server error, please try again later!'})
+    }
+}
+
+module.exports = {getNews, postNews};
