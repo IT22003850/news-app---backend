521158a04d78e9d0cd39f681f175a1c9c657296d
Thilina Thushara <thilina.sam.a7@gmail.com>
2025-08-16 15:08:12 +0530

    connect mongo DB

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const dotenv = require('dotenv').config();
 const newsRoutes = require('./routes/newsRoutes')
+const connectDB = require('./config/dbConfig')
 
 const PORT = process.env.PORT || 8000;
+
+connectDB();
 
 const app = express();
 
