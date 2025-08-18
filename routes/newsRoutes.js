c6ec5460528af6f57bb84f8fd86dfae053f4911a
Thilina Thushara <thilina.sam.a7@gmail.com>
2025-08-18 18:20:08 +0530

    new route added to update news

--- routes/newsRoutes.js
+++ routes/newsRoutes.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const newsController = require('../controllers/newsController')
+const newsController = require("../controllers/newsController");
 
-router.get('/', newsController.getNews)
-router.post('/',newsController.postNews)
+router.get("/", newsController.getNews);
+router.post("/", newsController.postNews);
+router.put("/:id", newsController.updateNews);
 
 module.exports = router;
